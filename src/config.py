87d9dda2cb5d1e3a0736874d4c7adf61a1191d4c
shaojieMike <44373239+Kirrito-k423@users.noreply.github.com>
2022-06-21 13:38:20 +0800

    finish ver1

---
+++ src/config.py
@@ -0,0 +1,11 @@
+programPath='E:\gamePojie\\NaiRen2\A3285 v2.0.2_yuzuEA2077\yuzu.exe'
+regexName = "uzu"
+
+ipadResolutionRatioX=2388
+ipadResolutionRatioY=1688
+
+positionsX=100
+positionsY=0
+
+DisplayX=2560
+DisplayY=1440
---
+++ src/main.py
@@ -0,0 +1,68 @@
+from time import time
+from turtle import setposition
+from tsjPython.tsjCommonFunc import *
+from config import *
+import re
+import time
+
+import win32con
+import win32gui
+import win32com.client
+
+def openTarget(reName):
+    import os
+    if searchRegexProcess(reName)==None:
+        os.startfile(programPath)
+        passPrint('try to Open Program')
+    else:
+        passPrint('Already Open Program')
+
+def searchRegexProcess(reName):
+    hWndList = []
+    win32gui.EnumWindows(lambda hWnd, param: param.append(hWnd), hWndList)
+    # print(hWndList)
+    
+    for hwnd in hWndList:
+        clsname = win32gui.GetClassName(hwnd)
+        title = win32gui.GetWindowText(hwnd)
+        if(re.match("(.)*{}(.)*".format(reName), clsname, re.IGNORECASE) or re.match("(.)*{}(.)*".format(reName), title, re.IGNORECASE)):
+            print("~~~~~~~~~~~")
+            print(clsname)
+            print(title)
+            return hwnd
+    return None
+
+def setPositionSize(x,y,height,width,reName):
+    count=0
+    while count==0:
+        hwnd=searchRegexProcess(reName)
+        if hwnd!=None:
+            count+=1
+            print("height {} width {}".format(height, width))
+            win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, x,
+                                y, width, height, win32con.SWP_SHOWWINDOW)
+            win32gui.BringWindowToTop(hwnd)
+            # 先发送一个alt事件，否则会报错导致后面的设置无效：pywintypes.error: (0, 'SetForegroundWindow', 'No error message is available')
+            shell = win32com.client.Dispatch("WScript.Shell")
+            shell.SendKeys('%')
+            win32gui.SetForegroundWindow(hwnd)
+            passPrint('Open Program')
+        else:
+            errorPrint('wait Program to Open')
+            time.sleep(1)
+    
+
+    # # 找出窗体的编号：
+    # QQwin = win32gui . FindWindow("TXGuiFoundation", "QQ")  # 写类和标题，中间逗号隔开
+    # # 控制窗体的位置和大小：
+    # win32gui.SetWindowPos(QQwin, win32con.HWND_TOPMOST, 100,
+    #                     100, 600, 600, win32con.SWP_SHOWWINDOW)
+def calculateXY():
+    scale=max(ipadResolutionRatioX/DisplayX,ipadResolutionRatioY/DisplayY)
+    return [int(ipadResolutionRatioX/scale),int(ipadResolutionRatioY/scale)]
+
+if __name__ == "__main__":
+    openTarget(regexName)
+    [allowableX,allowableY]=calculateXY()
+    setPositionSize(positionsX,positionsY,allowableY,allowableX,regexName)
+    
